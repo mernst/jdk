21c06978c0a5effc2c8e7d3df5ee959d73e45a7c
Michael Ernst <mernst@cs.washington.edu>
2026-07-13 13:21:02 -0700

    Add `import` statements

--- src/java.base/share/classes/java/util/concurrent/CopyOnWriteArraySet.java
+++ src/java.base/share/classes/java/util/concurrent/CopyOnWriteArraySet.java
@@ -49,6 +49,7 @@ import org.checkerframework.checker.signedness.qual.PolySigned;
 import org.checkerframework.checker.signedness.qual.UnknownSignedness;
 import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.dataflow.qual.SideEffectFree;
+import org.checkerframework.dataflow.qual.SideEffectsOnly;
 import org.checkerframework.framework.qual.DoesNotUnrefineReceiver;
 
 import java.util.AbstractSet;
